a8f58fa5218991c0074ffe7010eb244efe7af71d
Dídac Padilla <didacpadilla20@gmail.com>
2019-11-07 00:02:10 +0100

    Add Portal Gun Add both portals Added Portal assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [HideInInspector] public static GameManager instance { get; private set; }
+
+    [HideInInspector] private GameObject bluePortal;
+    [HideInInspector] private GameObject orangePortal;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+
+        instance = this;
+    }
+
+    public void ChangeBluePortal(GameObject newPortal)
+    {
+        if(bluePortal != null)
+            Destroy(bluePortal);
+
+        bluePortal = newPortal;
+    }
+
+    public void ChangeOrangePortal(GameObject newPortal)
+    {
+        if(orangePortal != null)
+            Destroy(orangePortal);
+
+        orangePortal = newPortal;
+    }
+
+}
---
+++ Assets/Scripts/PortalGun.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalGun : MonoBehaviour
+{
+    [SerializeField] private GameObject blueParticles = null;
+    [SerializeField] private GameObject orangeParticles = null;
+    [SerializeField] private GameObject bluePortalPrefab = null;
+    [SerializeField] private GameObject orangePortalPrefab = null;
+
+    [HideInInspector] private GameManager gm;
+
+    private void Start()
+    {
+        gm = GameManager.instance;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+            {
+                if (hit.transform.tag.Equals("Printable"))
+                {
+                    Destroy(Instantiate(blueParticles, hit.point, Quaternion.identity), .5f);
+                    gm.ChangeBluePortal(CreatePortal(bluePortalPrefab, hit));
+                }
+            }
+
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+            {
+                if (hit.transform.tag.Equals("Printable"))
+                {
+
+                    Destroy(Instantiate(orangeParticles, hit.point, Quaternion.identity), .5f);
+                    gm.ChangeOrangePortal(CreatePortal(orangePortalPrefab, hit));
+                }
+            }
+
+        }
+    }
+
+    private GameObject CreatePortal(GameObject portalPrefab, RaycastHit hit)
+    {
+        GameObject portal = Instantiate(portalPrefab, hit.point, Quaternion.identity);
+
+        portal.transform.forward = hit.normal;
+
+        return portal;
+    }
+
+}
